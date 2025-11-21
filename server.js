996535389dffd567849c36690b1e53eeabd56907
saqlain7903 <mdsaqlainraza92@gmail.com>
2025-11-21 13:18:39 +0530

    initial backend deploy

--- server.js
+++ server.js
@@ -1,44 +1,50 @@
-
 import express from "express";
 import multer from "multer";
 import cors from "cors";
 import QRCode from "qrcode";
 import path from "path";
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { v4 as uuidv4 } from "uuid";
 
-const NGROK_URL = " https://nonascetical-subdermic-edris.ngrok-free.dev";
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const NGROK_URL = process.env.NGROK_URL;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "uploads/"),
-    filename: (req, file, cb) => cb(null, uuidv4() + path.extname(file.originalname)),
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) =>
+    cb(null, uuidv4() + path.extname(file.originalname)),
 });
 
 const upload = multer({ storage });
+
 app.post("/api/files/upload", upload.single("image"), async (req, res) => {
-    try {
-        if (!req.file)
-            return res.status(400).json({ error: "No file uploaded" });
+  try {
+    if (!req.file)
+      return res.status(400).json({ error: "No file uploaded" });
 
-        const imageUrl = `${NGROK_URL}/uploads/${req.file.filename}`;
-        const qrCodeBase64 = await QRCode.toDataURL(imageUrl);
+    const imageUrl = `${NGROK_URL}/uploads/${req.file.filename}`;
+    const qrCodeBase64 = await QRCode.toDataURL(imageUrl);
 
-        res.json({
-            success: true,
-            imageUrl,
-            qrCode: qrCodeBase64,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Server error" });
-    }
+    res.json({
+      success: true,
+      imageUrl,
+      qrCode: qrCodeBase64,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
